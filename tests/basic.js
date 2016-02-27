0b93a3621b13e6f528fe791231d88873e6f990e2
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2016-02-27 22:44:05 +0100

    basic tests

--- tests/basic.js
+++ tests/basic.js
@@ -21,6 +21,94 @@ describe("Check server ", function() {
     var request = new Request();
     request.getRequest('http://localhost:3000/item', {}, function(err, res) {
       expect(err).to.not.exist;
+      expect(query).to.be.empty;
+      expect(body).to.be.empty;
+      expect(params).to.be.empty;
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at get request with param", function(done) {
+    var request = new Request();
+    request.getRequest('http://localhost:3000/item/1', {}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(query).to.be.empty;
+      expect(body).to.be.empty;
+      expect(params.param).to.be.equal('1');
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at get request with param", function(done) {
+    var request = new Request();
+    request.getRequest('http://localhost:3000/item/1', { qparam: 'test'}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(query.qparam).to.be.equal('test');
+      expect(body).to.be.empty;
+      expect(params.param).to.be.equal('1');
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at post request", function(done) {
+    var request = new Request();
+    request.postRequest('http://localhost:3000/item', {}, {}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(query).to.be.empty;
+      expect(body).to.be.empty;
+      expect(params).to.be.empty;
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at post request", function(done) {
+    var request = new Request();
+    request.postRequest('http://localhost:3000/item', { pparam: 'test2'}, { qparam: 'test1'}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(query.qparam).to.be.equal('test1');
+      expect(params).to.be.empty;
+      expect(body.pparam).to.be.equal('test2');
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at put request", function(done) {
+    var request = new Request();
+    request.putRequest('http://localhost:3000/item/1', {}, {}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(params.param).to.be.equal('1');
+      expect(query).to.be.empty;
+      expect(body).to.be.empty;
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at put request", function(done) {
+    var request = new Request();
+    request.putRequest('http://localhost:3000/item/1', { pparam: 'test2'}, { qparam: 'test1'}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(params.param).to.be.equal('1');
+      expect(query.qparam).to.be.equal('test1');
+      expect(body.pparam).to.be.equal('test2');
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at delete request", function(done) {
+    var request = new Request();
+    request.deleteRequest('http://localhost:3000/item/1', {}, {}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(res.status).to.be.equal(200);
+      done();
+    })
+  });
+  it("should return status code 200 at delete request", function(done) {
+    var request = new Request();
+    request.deleteRequest('http://localhost:3000/item/1', { pparam: 'test2'}, { qparam: 'test1'}, function(err, res) {
+      expect(err).to.not.exist;
+      expect(params.param).to.be.equal('1');
+      expect(query.qparam).to.be.equal('test1');
+      expect(body.pparam).to.be.equal('test2');
       expect(res.status).to.be.equal(200);
       done();
     })
